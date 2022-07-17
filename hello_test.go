a74650f7040041afb0487624ecd31b033a819f8b
FahrezaIsnanto <mfahrezaisnanto@students.undip.ac.id>
2022-07-17 13:08:38 +0700

    add success test case 1

--- hello_test.go
+++ hello_test.go
@@ -12,7 +12,7 @@ import (
 func TestHelloName(t *testing.T) {
 	name := "Gladys"
 	want := regexp.MustCompile(`\b` + name + `\b`)
-	msg, err := golanggreetings.Hello("Anton")
+	msg, err := golanggreetings.Hello("Gladys")
 	if !want.MatchString(msg) || err != nil {
 		t.Fatalf(`Hello("Gladys") = %q, %v, want match for %#q, nil`, msg, err, want)
 	}
